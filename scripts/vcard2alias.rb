ee5c0e1d4c2b8973f0ec2ff433a24881990e91b1
Stephen Paul Weber <singpolyma@singpolyma.net>
2009-05-31 22:54:29 -0700

    Added a vcard2aliases converter to compliment gmail2mutt

---
+++ scripts/vcard2alias.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+all_emails = []
+
+STDIN.read.gsub(/\r\n/,"\n").scan(/BEGIN:VCARD\n([^\f]+?)END:VCARD\n/).each do |vcf|
+	emails = []
+	fn = nil
+	vcf[0].each_line do |line|
+		line = line.chomp.split(/:/)
+		line[0] = line[0].split(/;/).shift
+		line[1].gsub!(/\\(.)/, '\1') if line[1]
+		emails << line[1] if line[0] == 'EMAIL'
+		fn = line[1] if line[0] == 'FN'
+	end
+	emails.each do |email|
+		next if all_emails.index(email)
+		puts "alias \"#{email}\" \"#{fn || email}\" <#{email}>"
+		puts "alias \"#{fn.gsub(/\s+/,'')}\" \"#{fn}\" <#{email}>" if fn
+		all_emails << email
+	end
+end
